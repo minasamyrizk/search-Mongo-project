f650e59c733b0d3e7424d4d8876a5fc11e47d09c
minasamyrizk <92720068+minasamyrizk@users.noreply.github.com>
2021-10-18 18:25:47 +0200

    Update Product.js
    add next/image to show product image

--- components/Product.js
+++ components/Product.js
@@ -1,5 +1,5 @@
 import React from "react";
-//import Image from "next/image";
+import Image from "next/image";
 import Link from "next/link";
 
 const Product = ({ product }) => {
@@ -7,6 +7,13 @@ const Product = ({ product }) => {
     <Link href={`/products/${product._id}`}>
       <div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden cursor-pointer hover:shadow-2xl transition">
         <div className="flex items-end justify-end h-56 w-full bg-cover relative">
+        <Image
+            src={product.image_urls[0]}
+            alt={product.product_title}
+            layout="fill"
+            objectFit="cover"
+            className="absolute z-0"
+          />
         </div>
         <div className="px-5 py-3">
           <h3 className="text-gray-700 uppercase">{product.product_title}</h3>
