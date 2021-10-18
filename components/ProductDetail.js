970f7530c8774c330e0959ec5a6029cc4c61f62b
minasamyrizk <92720068+minasamyrizk@users.noreply.github.com>
2021-10-18 18:27:20 +0200

    Update ProductDetail.js
    add next/image to show product image when selected

--- components/ProductDetail.js
+++ components/ProductDetail.js
@@ -1,12 +1,19 @@
 import React from "react";
-//import Image from "next/image";
+import Image from "next/image";
 import { PlusCircleIcon, MinusCircleIcon } from "@heroicons/react/outline";
 
 const ProductDetail = ({ product }) => {
   return (
     <div className="md:flex md:items-center">
       <div className="w-full h-64 md:w-1/2 lg:h-96 relative">
-        
+    <Image
+            src={product.image_urls[0]}
+            alt={product.product_title}
+            layout="fill"
+            objectFit="cover"
+            className="absolute z-0"
+          />
+    
       </div>
       <div className="w-full max-w-lg mx-auto mt-5 md:ml-8 md:mt-0 md:w-1/2">
         <h3 className="text-gray-700 uppercase text-lg">{product.product_title}</h3>
